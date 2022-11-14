399c36f793b120cead4745ef4de25aa2a0b186d8
KanitaNiki <kanitaafrin@gmail.com>
2022-11-14 15:53:43 -0500

    Scroll event, css updates
    All features are usable across modern desktop, tablet, and phone browsers.
    Styling has been added for active states.
    
    Code is formatted with consistent, logical, and easy-to-read formatting.
    
    README file updated.

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Landing Page Project
 
 ## Table of Contents
-* [Project Description](#description)
-* [Usage](#usage)
-* [Instructions](#instructions)
 
+- [Project Description](#description)
+- [Usage](#usage)
+- [Instructions](#instructions)
 
 ## Project Description
 
-This project implements a dynamic website with interactive navigation menu. There are four content sections in the webpage and the user can click on the navigation menu items to automatically scroll to each section. The section selected will be highlighted. This dynamic webpage has been created using Javascript along with the help of starter HTML and CSS codes provided by Udacity. 
+This project implements a dynamic website with interactive navigation menu. There are four content sections in the webpage and the user can click on the navigation menu items to automatically scroll to each section. The section selected will be highlighted.The navigation menu items are highlighted accordingly as user scrolls through the page. This dynamic webpage has been created using Javascript along with the help of starter HTML and CSS codes provided by Udacity.
 
 ## Usage
-This interactive website can be used as a landing page of a blog post, company home page or a product page. 
 
+This interactive website can be used as a landing page of a blog post, company home page or a product page.
 
 ## Instructions
-To access the website, open the `index.html` page in the web browser. The styling can be found in `css/style.css` file and the app's functionality JS code can be found in `js/app.js` file. 
 
-Click on the nav menu items to allow smooth scrolling to the section selected. 
+To access the website, open the `index.html` page in the web browser. The styling can be found in `css/style.css` file and the app's functionality JS code can be found in `js/app.js` file.
 
+Click on the nav menu items to allow smooth scrolling to the section selected and scroll manually through the page to see the appropriate section name highlighted at the top.
--- js/app.js
+++ js/app.js
@@ -1,106 +1,84 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
-*/
-
-const sections = document.getElementsByTagName('section');
-const ulEl= document.getElementById("navbar__list");
-
-/**
- * End Global Variables
-*/
-
-
-
-/**
- * Build the nav as unordered list
- * Use appenChild to dynamically build nav
  */
-
-
-for (section of sections){
-    let sectionAttr = section.getAttribute('data-nav');
-    let sectionId = section.getAttribute('id');
-    const listItem = document.createElement('li');
-    const anchor = document.createElement('a');
-    anchor.setAttribute('href','#'+ sectionId);
-    anchor.textContent = sectionAttr;
-    listItem.appendChild(anchor);
-    ulEl.appendChild(listItem);  
-
-}
-
-/**
- * Listen to click event
- * Stop default event from occuring 
- */
-
-
-const listAnchors = document.querySelectorAll('a') 
-
-for (listAnchor of listAnchors){
-    listAnchor.addEventListener('click', function(evt) {
-        evt.preventDefault();
-        const href = this.getAttribute("href");
-        const offsetTop = document.querySelector(href).offsetTop;
-        scroll({
-            top: offsetTop,
-            behavior: "smooth"
-        })
-
-    })
-
-}
-
-/**
- * Scroll to section on link click
- * Add class 'active' to section when near top of viewport
- * Set sections as active
- */
-
-    window.addEventListener("scroll",function(event){ 
-        for (section of sections){
-            const sectionTop = section.getBoundingClientRect().top + 300;
-            const windowHeight = this.innerHeight;
-
-            if(sectionTop< innerHeight){
-                const current = document.getElementsByClassName("active-class");
-            
-
-                if(current.length > 0) {
-                    current[0].classList.remove("active-class");
-                }
-                section.classList.add('active-class');
-            }
-
-            
-        }
-    })
+ const sections = document.getElementsByTagName('section');
+ const ulEl = document.getElementById('navbar__list');
+  
+ /**
+  * Build the nav as unordered list
+  * Use appenChild to dynamically build nav
+  */
  
-
-        
-   
-
-
-
-
-
+ 
+ for (section of sections) {
+     let sectionAttr = section.getAttribute('data-nav');
+     let sectionId = section.getAttribute('id');
+     const listItem = document.createElement('li');
+     const anchor = document.createElement('a');
+     anchor.classList.add('menu__link');
+     anchor.setAttribute('href', '#' + sectionId);
+     anchor.textContent = sectionAttr;
+     listItem.appendChild(anchor);
+     ulEl.appendChild(listItem);
+ 
+ }
+ 
+ /**
+  * Listen to click event
+  * Stop default event from occuring 
+  * Add class 'active-item' to nav list item following click event
+  * Scroll to section on link click
+  */
+ 
+ 
+ const listAnchors = document.querySelectorAll('a')
+ 
+ for (listAnchor of listAnchors) {
+     listAnchor.addEventListener('click', function(evt) {
+         evt.preventDefault();
+         const href = this.getAttribute('href');
+         const offsetTop = document.querySelector(href).offsetTop;
+         scroll({
+             top: offsetTop,
+             behavior: "smooth"
+         })
+ 
+     })
+ 
+ }
+ 
+ /**
+  * Add class 'active-class' to section when near top of viewport
+  * Set and remove sections as 'active-class'
+  * Add class 'active-listitem' to nav list item following scroll event
+  * Set and remove nav list items as 'active-listitem'
+  */
+ 
+ window.addEventListener('scroll', function(event) {
+     for (section of sections) {
+         const sectionTop = section.getBoundingClientRect().top + 300;
+         const windowHeight = this.innerHeight;
+ 
+         if (sectionTop < innerHeight) {
+             const current = document.getElementsByClassName('active-class');
+ 
+ 
+             if (current.length > 0) {
+                 current[0].classList.remove('active-class');
+             }
+             section.classList.add('active-class');
+ 
+             const sectionId = section.getAttribute('id');
+             const activeAnchor = document.querySelectorAll(`a[href='#${sectionId}']`);
+             const currentAnchor = document.getElementsByClassName('active-listitem')
+             if (currentAnchor.length > 0) {
+                 currentAnchor[0].classList.remove('active-listitem')
+             }
+             activeAnchor[0].classList.add('active-listitem')
+ 
+         }
+ 
+ 
+ 
+     }
+ })
